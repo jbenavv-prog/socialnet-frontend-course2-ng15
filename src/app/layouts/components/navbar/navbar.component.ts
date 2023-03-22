0ffa7859a5d48f510b33ede86399ee0804958dbe
Julian Camilo Benavides Vanegas <julian.127@teleperformance.com>
2023-03-21 20:29:52 -0500

    photo profile

--- src/app/layouts/components/navbar/navbar.component.ts
+++ src/app/layouts/components/navbar/navbar.component.ts
@@ -1,5 +1,6 @@
 import { Component } from '@angular/core';
 import { AuthService } from 'src/app/services/auth.service';
+import { ProfileService } from 'src/app/services/profile.service';
 
 @Component({
   selector: 'app-navbar',
@@ -8,11 +9,15 @@ import { AuthService } from 'src/app/services/auth.service';
 })
 export class NavbarComponent {
   user: any;
+  profile: any;
 
-  constructor(private authService: AuthService) {}
+  constructor(
+    private authService: AuthService,
+    private profileService: ProfileService
+  ) {}
 
   ngOnInit() {
-    this.getUser()
+    this.getUser();
   }
 
   logout() {
@@ -22,11 +27,37 @@ export class NavbarComponent {
   getUser() {
     this.user = this.authService.getUser();
     console.log(this.user);
-    
   }
 
-  uploadImage(e:any){
+  uploadImage(e: any) {
+    console.log('change photo called');
     const file = e.target.files[0];
-    console.log(file);
+
+    if (!file) {
+      return;
+    }
+
+    const formData = new FormData();
+    formData.append('photo', file);
+    this.profileService.uploadPhotoProfile(formData).subscribe({
+      next: (data) => {
+        console.log(data);
+        this.ngOnInit();
+      },
+      error: (error) => {
+        console.log(error);
+      },
+    });
+  }
+
+  getProfile() {
+    this.profileService.getProfile().subscribe({
+      next: (response: any) => {
+        this.profile = response.data;
+      },
+      error: (error) => {
+        console.log(error);
+      },
+    });
   }
 }

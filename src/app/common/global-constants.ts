efa6e47b8ac4bc26ae666f4d3a730c73d2d27c5b
Julian Camilo Benavides Vanegas <julian.127@teleperformance.com>
2023-03-22 15:55:48 -0500

    github pages

--- src/app/common/global-constants.ts
+++ src/app/common/global-constants.ts
@@ -1,4 +1,4 @@
 export class GlobalConstants{
-    public static apiURL: string = "http://localhost:3000/api/v1";
+    public static apiURL: string = "https://calm-rose-goose-hose.cyclic.app/api/v1";
     // public static privateKey: string = "dasjhcxzc4234235254#$%##21"
 }
